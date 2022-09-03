a05d180e747b5cbb001e4ba21671556448fb51b5
Kevin A. Villeda <vh21011@ues.edu.sv>
2022-09-02 21:31:02 -0600

    Creando la guia 02

---
+++ guia02_2022/src/main/java/ues/occ/edu/sv/ingenieria/prn335/guia02_2022/Guia02_2022.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package ues.occ.edu.sv.ingenieria.prn335.guia02_2022;
+
+/**
+ *
+ * @author Kevin
+ */
+public class Guia02_2022 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
